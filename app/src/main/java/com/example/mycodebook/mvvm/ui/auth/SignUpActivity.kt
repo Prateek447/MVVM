e0fce1ad7f2816ac00e5e01c6f180d172fede32a
Prateek <yprateek51@gmail.com>
2021-05-20 17:50:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mycodebook/mvvm/ui/auth/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mycodebook.mvvm.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mycodebook.mvvm.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
